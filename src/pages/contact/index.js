6fd89ce5f5d0f9ea8c0e9d4fd6f569c47a0fbe4f
lwa-imp <l.waclawczyk@imp-ag.de>
2024-07-08 08:35:05 -0400

    all wide separators

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -73,9 +73,9 @@ export const ContactUs = () => {
           <meta name="description" content={meta.description} />
         </Helmet>
         <Row className="mb-5 mt-3 pt-md-3">
-          <Col lg="8">
+          <Col lg="12">
             <h1 className="display-4 mb-4">Contact Me</h1>
-            <hr className="t_border my-4 ml-0 text-left" />
+            <hr className="separator-line" />
           </Col>
         </Row>
         <Row className="sec_sp">
